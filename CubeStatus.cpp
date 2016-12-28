1c58d2cad5f7dc5cacb7c1b2b636ac11eb1992b4
BenjaminPMLovegood <benjaminpmlee@gmail.com>
2016-12-29 01:12:32 +0800

    Cube finished

--- CubeStatus.cpp
+++ CubeStatus.cpp
@@ -1,7 +1,7 @@
+#include <algorithm>
+
 //UBRFLD
-
 class Cube;
-
 typedef void (Cube::*func)();
 
 enum class Face {
@@ -20,17 +20,6 @@ enum class Face {
 };
 
 class Cube {
-    typedef char face[9];
-
-    int neighborFace[6][12][2] = {
-        {{1, 2}, {1, 1}, {1, 0}, {2, 2}, {2, 1}, {2, 0}, {3, 2}, {3, 1}, {3, 0}, {4, 2}, {4, 1}, {4, 0}},
-        {{0, 2}, {0, 1}, {0, 0}, {4, 0}, {4, 3}, {4, 6}, {5, 6}, {5, 7}, {5, 8}, {2, 8}, {2, 5}, {2, 2}},
-        {{0, 8}, {0, 5}, {0, 2}, {1, 0}, {1, 3}, {1, 6}, {5, 8}, {5, 5}, {5, 2}, {3, 8}, {3, 5}, {3, 2}},
-        {{0, 6}, {0, 7}, {0, 8}, {2, 0}, {2, 3}, {2, 6}, {5, 2}, {5, 1}, {5, 0}, {4, 8}, {4, 5}, {4, 2}},
-        {{0, 0}, {0, 3}, {0, 6}, {3, 0}, {3, 3}, {3, 6}, {5, 0}, {5, 3}, {5, 6}, {1, 8}, {1, 5}, {1, 2}},
-        {{4, 6}, {4, 7}, {4, 8}, {3, 6}, {3, 7}, {3, 8}, {2, 6}, {2, 7}, {2, 8}, {1, 6}, {1, 7}, {1, 8}}
-    };
-
     void rc(int face);
     void ri(int face);
 
@@ -50,20 +39,53 @@ public :
 
     const char* operator [] (const int & b) const;
 
-    func f[12] = {
+    func fn[12] = {
         U, B, R, F, L, D,    
         Ui, Bi, Ri, Fi, Li, Di
     };
 
     void Rotate(Face fa) {
-        (this->*f[(int)fa])();
+        (this->*fn[(int)fa])();
+    }
+
+private :
+    typedef char face[9];
+
+    face f[6];
+
+    int neighborFace[6][12][2] = {
+        {{1, 2}, {1, 1}, {1, 0}, {2, 2}, {2, 1}, {2, 0}, {3, 2}, {3, 1}, {3, 0}, {4, 2}, {4, 1}, {4, 0}},
+        {{0, 2}, {0, 1}, {0, 0}, {4, 0}, {4, 3}, {4, 6}, {5, 6}, {5, 7}, {5, 8}, {2, 8}, {2, 5}, {2, 2}},
+        {{0, 8}, {0, 5}, {0, 2}, {1, 0}, {1, 3}, {1, 6}, {5, 8}, {5, 5}, {5, 2}, {3, 8}, {3, 5}, {3, 2}},
+        {{0, 6}, {0, 7}, {0, 8}, {2, 0}, {2, 3}, {2, 6}, {5, 2}, {5, 1}, {5, 0}, {4, 8}, {4, 5}, {4, 2}},
+        {{0, 0}, {0, 3}, {0, 6}, {3, 0}, {3, 3}, {3, 6}, {5, 0}, {5, 3}, {5, 6}, {1, 8}, {1, 5}, {1, 2}},
+        {{4, 6}, {4, 7}, {4, 8}, {3, 6}, {3, 7}, {3, 8}, {2, 6}, {2, 7}, {2, 8}, {1, 6}, {1, 7}, {1, 8}}
+    };
+
+    int innerFaceOrder[8] = {
+        0, 1, 2, 5, 8, 7, 6, 3
+    };
+public :
+    Cube() {;}
+    Cube(char ** F) {
+        for (int i = 0; i < 6; ++i) for (int j = 0; j < 9; ++j) f[i][j] = F[i][j];
     }
 };
 
 void Cube::rc(int face) {
-    
+    int (*nF)[2] = neighborFace[face];
+
+    for (int i = 0; i < 3; ++i) for (int j = 11; j >= 1; --j) std::swap(f[nF[j][0]][nF[j][1]], f[nF[j - 1][0]][nF[j - 1][1]]);
+    for (int i = 0; i < 2; ++i) for (int j = 7; j >= 1; --j) std::swap(f[face][innerFaceOrder[j]], f[face][innerFaceOrder[j - 1]]);
 }
 
 void Cube::ri(int face) {
+    int (*nF)[2] = neighborFace[face];
     
+    for (int i = 0; i < 3; ++i) for (int j = 1; j < 12; ++j) std::swap(f[nF[j][0]][nF[j][1]], f[nF[j - 1][0]][nF[j - 1][1]]);
+    for (int i = 0; i < 2; ++i) for (int j = 1; j < 8; ++j) std::swap(f[face][innerFaceOrder[j]], f[face][innerFaceOrder[j - 1]]);
+}
+
+const char* Cube::operator [] (const int & b) const {
+    return (const char*)f[b];
 }
--- test/cubestatus.test.cpp
+++ test/cubestatus.test.cpp
@@ -1,5 +1,49 @@
+
 #include "../cubestatus.cpp"
 
+#include <cstdio>
+using namespace std;
+
+void printCube(const Cube & cu) {
+    printf("   %c%c%c\n", cu[0][0], cu[0][1], cu[0][2]);
+    printf("   %c%c%c\n", cu[0][3], cu[0][4], cu[0][5]);
+    printf("   %c%c%c\n", cu[0][6], cu[0][7], cu[0][8]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c\n", cu[4][0], cu[4][1], cu[4][2], cu[3][0], cu[3][1], cu[3][2], cu[2][0], cu[2][1], cu[2][2], cu[1][0], cu[1][1], cu[1][2]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c\n", cu[4][3], cu[4][4], cu[4][5], cu[3][3], cu[3][4], cu[3][5], cu[2][3], cu[2][4], cu[2][5], cu[1][3], cu[1][4], cu[1][5]);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c\n", cu[4][6], cu[4][7], cu[4][8], cu[3][6], cu[3][7], cu[3][8], cu[2][6], cu[2][7], cu[2][8], cu[1][6], cu[1][7], cu[1][8]);
+    printf("   %c%c%c\n", cu[5][0], cu[5][1], cu[5][2]);
+    printf("   %c%c%c\n", cu[5][3], cu[5][4], cu[5][5]);
+    printf("   %c%c%c\n", cu[5][6], cu[5][7], cu[5][8]);
+}
+
+char v[6][10];
+char *vv[] = { v[0], v[1], v[2], v[3], v[4], v[5] };
+
+char ctf[256];
 int main() {
-    
+    printf("input a cube (UBRFLD) :\n");
+
+    for (int i = 0; i < 6; ++i) {
+        printf(">> ");
+        scanf("%s", v[i]);
+    }
+
+    Cube c(vv);
+
+    printCube(c);
+
+    char cmd[100]; ctf['U'] = 0; ctf['B'] = 1; ctf['R'] = 2; ctf['F'] = 3; ctf['L'] = 4; ctf['D'] = 5; 
+    for (;;) {
+        printf(">> ");
+
+        if (!~scanf("%s", cmd)) { break; }
+
+        int act = ctf[cmd[0]]; if (cmd[1] == 'i') act += 6;
+
+        c.Rotate((Face)act);
+
+        printCube(c);
+    }
+
+    return 0;
 }
